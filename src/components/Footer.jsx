6460b19e6119c69a14ea51d5fc177856d47a749b
Juan Andres Parra <metalnerd777@gmail.com>
2021-11-11 00:57:07 -0300

    pagina principal terminada

---
+++ src/components/Footer.jsx
@@ -0,0 +1,134 @@
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@mui/icons-material";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+const ListItem = styled.li`
+  width: 50%;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+const ContactItem = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+  align-items: center;
+`;
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Logo = styled.h1``;
+
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  cursor: pointer;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>My Store ©</Logo>
+        <Description>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
+          praesentium voluptate accusantium earum eaque. Quos, distinctio
+          tempore. Placeat odio perspiciatis earum corporis culpa eveniet
+          doloremque possimus? Distinctio recusandae labore quibusdam.
+        </Description>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>About Us</ListItem>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accesories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} />
+          3777 Poplar Avenue, CA
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} /> +1 234 56 78
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} /> contact@mystore.com
+        </ContactItem>
+        <Payment src="https://basyspro.com/wp-content/uploads/2017/02/Credit-Card-Logos.jpg" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
